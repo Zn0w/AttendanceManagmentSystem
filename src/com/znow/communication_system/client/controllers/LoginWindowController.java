5081f9dcdf37d08eb929beb268e9e4b88c5d0547
Daniil Blagiy <blagiy.daniil@gmail.com>
2017-10-14 10:09:20 +0300

    added login window

---
+++ src/com/znow/communication_system/client/controllers/LoginWindowController.java
@@ -0,0 +1,9 @@
+package com.znow.communication_system.client.controllers;
+
+public class LoginWindowController {
+	
+	public void onConnectButton() {
+		System.out.println("Connect button has been pressed!");
+	}
+	
+}
--- src/com/znow/communication_system/client/gui/ClientWindow.java
+++ src/com/znow/communication_system/client/gui/ClientWindow.java
@@ -1,6 +1,16 @@
 package com.znow.communication_system.client.gui;
 
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import com.znow.communication_system.client.controllers.LoginWindowController;
 
 @SuppressWarnings("serial")
 public class ClientWindow extends JFrame {
@@ -20,10 +30,26 @@ public class ClientWindow extends JFrame {
 		JPanel root = new JPanel();
 		root.setLayout(new BoxLayout(root, BoxLayout.Y_AXIS));
 		
-		for (int i = 0; i < 10; i++) {
-			JLabel label = new JLabel("Hello!");
-			root.add(label);
-		}
+		JLabel loginLabel = new JLabel("Enter your login:");
+		root.add(loginLabel);
+		
+		JTextField loginTxt = new JTextField();
+		root.add(loginTxt);
+		
+		JLabel passwordLabel = new JLabel("Enter your password:");
+		root.add(passwordLabel);
+		
+		JTextField passwordTxt = new JTextField();
+		root.add(passwordTxt);
+		
+		JButton connectButton = new JButton("Log in");
+		connectButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				new LoginWindowController().onConnectButton();
+			}
+		});
+		root.add(connectButton);
 		
 		setContentPane(root);
 		pack();
