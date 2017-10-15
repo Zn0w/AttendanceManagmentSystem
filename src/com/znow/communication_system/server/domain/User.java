96e84d47fe8e41b8ecd12427dbdd1a6885edf16f
Daniil Blagiy <blagiy.daniil@gmail.com>
2017-10-15 14:48:45 +0300

    added User class in domain layer

--- src/com/znow/communication_system/server/domain/User.java
+++ src/com/znow/communication_system/server/domain/User.java
@@ -2,6 +2,26 @@ package com.znow.communication_system.server.domain;
 
 public class User {
 	
+	private String login;
+	private String password;
+	private String name;
 	
+	public User(String login, String password, String name) {
+		this.login = login;
+		this.password = password;
+		this.name = name;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public String getName() {
+		return name;
+	}
 	
 }
